ed696a659db80b1731dac84b6cb243c47302b8a8
karenfrilya97 <karenfrilya97@gmail.com>
2017-07-04 21:11:54 +0700

    Bounded map to NUS Changed Directions page UI Added Time Picker for Departure Time in Directions page Moved search bar in Maps page slightly to the left

--- app/src/main/java/com/example/bryan/nusmapsv2/DirectionsActivity.java
+++ app/src/main/java/com/example/bryan/nusmapsv2/DirectionsActivity.java
@@ -1,12 +1,19 @@
 package com.example.bryan.nusmapsv2;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class DirectionsActivity extends AppCompatActivity {
 
@@ -20,9 +27,9 @@ public class DirectionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
 
-        editOrigin = (EditText) findViewById(R.id.editFrom);
-        editDestination = (EditText) findViewById(R.id.editTo);
-        btnWalk = (Button) findViewById(R.id.buttonWalk);
+        editOrigin = (EditText) findViewById(R.id.startingPoint);
+        editDestination = (EditText) findViewById(R.id.destination);
+        btnWalk = (Button) findViewById(R.id.walk);
 
         btnWalk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +50,30 @@ public class DirectionsActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
